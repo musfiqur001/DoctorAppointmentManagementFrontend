c19478bd9b4dac818b41b040d712ad57d4208b15
Md. Musfiqur Rahman <projion@gmail.com>
2025-05-21 02:15:07 +0600

    layout implemented

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
     standalone: true,
     selector: 'app-footer',
     template: `<div class="layout-footer">
-        SAKAI by
-        <a href="https://primeng.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeNG</a>
+        Developed by
+        <a href="https://musfiqur001.github.io/" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Musfiqur Rahman</a>
     </div>`
 })
 export class AppFooter {}
