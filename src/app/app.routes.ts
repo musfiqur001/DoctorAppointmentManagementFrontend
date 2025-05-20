77e0569bdf71377b7f28a53b3d6cbd16de9b9f3f
Md. Musfiqur Rahman <projion@gmail.com>
2025-05-21 01:04:13 +0600

    layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AppLayout } from './layout/component/app.layout';
+import { HomeComponent } from './app-ui/components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppLayout,
+    children: [
+      { path: '', component: HomeComponent },
+      //{ path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
+      //{ path: 'documentation', component: Documentation },
+      //{ path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
+    ]
+  }
+  //{ path: 'landing', component: Landing },
+  //{ path: 'notfound', component: Notfound },
+  //{ path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
+  //{ path: '**', redirectTo: '/notfound' }
+];
